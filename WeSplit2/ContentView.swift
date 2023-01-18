1a9ec7c7dbdd2ec18f196d29789f8064af955001
wombus <peter.napoli@gmail.com>
2023-01-17 21:27:39 -0500

    Initial Commit

---
+++ WeSplit2/ContentView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
